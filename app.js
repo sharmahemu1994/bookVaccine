f0d68975aaff987ad8d32a8b83f913766fb1cb20
himanshu <hisharm4@cisco.com>
2021-05-09 19:38:28 +0530

    change in schedule vaccine

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const requestPromise = require('request-promise');
 const notifier = require('node-notifier');
 
-let date = '07-05-2021'; // current date
+// secret: 
+
+let date = '10-05-2021';
 let timer = 15000;
-let beneficiariesId = []; // beneficiaries_reference ids for whome you want to book the vaccine appointment
-let userAuthotisationToken = ``; // pass bearer token you ca get from the browser
+let beneficiariesId = [
+]; // add beneficiaries id
+
+let userAuthotisationToken = ``;
+ // add authentication bearer token can get
+
 
 const scheduleVaccine = async (data) => {
+	const {capacity, ...data2} = data;
 	let dataToSend = {
 		"dose": 1,
-		"beneficiaries": beneficiariesId,
-		...data
+		"beneficiaries": capacity > 1 ? beneficiariesId : [beneficiariesId[1]],
+		"captcha": 'Vfqbv', // tWy2p, RGcxH, r6Pzx
+		// "session_id": "b4e8e3aa-e398-4572-8385-09f7ede40c3c",
+		// "center_id": 572920,
+		// "slot": "05:26PM-05:56PM"
+		...data2
 	};
 	console.log('----dataToSend---', dataToSend);
-	return await requestPromise({
+	return  await requestPromise({
 		method: 'POST',
-		uri: `https://cdn-api.co-vin.in/api/v2/appointment/beneficiaries`,
+		uri: `https://cdn-api.co-vin.in/api/v2/appointment/schedule`,
 		headers: {
 			'authorization': userAuthotisationToken
 		},
@@ -23,14 +34,21 @@ const scheduleVaccine = async (data) => {
 		resolveWithFullResponse: true,
 		body: dataToSend
 	}).then(result => {
-		console.log('----dsadsadsadas-----', result.body)
+		console.log('---------', result.body)
 		return true
 	}).catch(err => {
 		console.log('------scheduleVaccine------', err.message);
-		console.log('------scheduleVaccine------', err);
 		return false;
 	});
 }
+
+const dummyRequestData = {
+	"session_id": "b4e8e3aa-e398-4572-8385-09f7ede40c3c",
+	"center_id": 572920,
+	"slot": "05:26PM-05:56PM",
+	capacity: 2
+};
+
 
 let checkAppointment = async () => {
 	let availableCentres = [];
@@ -45,18 +63,19 @@ let checkAppointment = async () => {
 	}).then(result => {
 		for (const item of result.body.centers) {
 			for (const item2 of item.sessions) {
-				if (item2.min_age_limit === 18 && item2.available_capacity > 0 && item2.vaccine == 'COVISHIELD') {
+				if (item2.min_age_limit === 18 && item2.available_capacity > 0) {
 					availableCentres.push({
 						session_id: item2.session_id,
-						slot: item2.slots[item2.slots.length - 1]
-					})
+						slot: item2.slots[item2.slots.length - 1],
+						center_id: item.center_id,
+						capacity: item2.available_capacity
+					});
 					console.log(item.name, item.pincode);
 				}
 			}
 		}
 		console.log('----availableCentres----', availableCentres);
 		return availableCentres;
-
 	}).catch(err => {
 		console.log(err.statusCode);
 		return availableCentres;
@@ -65,6 +84,11 @@ let checkAppointment = async () => {
 
 (
 	async function check() {
+		// try {
+		// 	await scheduleVaccine(dummyRequestData);
+		// }catch(error){
+		// 	console.error("error scheduling dummy request", error);
+		// }
 		let appointsments = await checkAppointment();
 		if (appointsments.length > 0) {
 			let datatosend = [];
@@ -83,9 +107,8 @@ let checkAppointment = async () => {
 				});
 				process.exit(1)
 			} else {
-				setTimeout(() => {
-					check();
-				}, timer);
+				process.exit(1)
+				check();
 			}
 		} else {
 			setTimeout(() => {
@@ -94,3 +117,23 @@ let checkAppointment = async () => {
 		}
 	}
 )();
+
+
+// api to get captcha
+const getCaptcha = async () =>{
+	let availableCentres = [];
+	return await requestPromise({
+		method: 'GET',
+		uri: `https://cdn-api.co-vin.in/api/v2/auth/getRecaptcha`,
+		headers: {
+			'authorization': userAuthotisationToken
+		},
+		json: true,
+		resolveWithFullResponse: true
+	}).then(result => {
+		console.log('----availableCentres----', result.body);
+	}).catch(err => {
+		console.log(err.statusCode);
+		return availableCentres;
+	});
+}
